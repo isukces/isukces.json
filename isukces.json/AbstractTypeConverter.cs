c815bad5c107684625304d2c51174fe69bd9d719
Piotr Stęclik <MrCompiler@cs2php.com>
2015-07-08 09:47:21 +0200

    AbstractTypeConverter that allows deserializing abstract types or interfaces

---
+++ isukces.json/AbstractTypeConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace isukces.json
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TConcrete"></typeparam>
+    /// <typeparam name="TAbstract"></typeparam>
+    public sealed class AbstractTypeConverter<TAbstract, TConcrete> : JsonConverter
+    {
+		#region Methods 
+
+		// Public Methods 
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(TAbstract);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return serializer.Deserialize<TConcrete>(reader);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            // nothing to do - just serialize
+            serializer.Serialize(writer, value);
+        }
+
+		#endregion Methods 
+    }
+}
---
+++ isukces.json/JsonExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace isukces.json
+{
+    /// <summary>
+    ///  Newtonsoft.Json objects extensions
+    /// </summary>
+    public static class JsonExtensions
+    {
+        /// <summary>
+        /// Appends AbstractTypeConverter that allows to deserialize abstract types or interfaces
+        /// </summary>
+        /// <param name="src"></param>
+        /// <typeparam name="TAbstract"></typeparam>
+        /// <typeparam name="TConcrete"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static JsonSerializer WithAbstractTypeConverter<TAbstract, TConcrete>(this JsonSerializer src)
+        {
+            if (src == null) throw new ArgumentNullException("src");
+            src.Converters.Add(new AbstractTypeConverter<TAbstract, TConcrete>());
+            return src;
+        }
+    }
+}
